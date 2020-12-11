c3e302f11bbd56ea9880c7b545a2b078a1d4fd87
MS <MS@DESKTOP-72BE4TC>
2020-12-11 21:45:01 +0900

    oJavaScirpt

---
+++ WebContent/ch01/example.js
@@ -0,0 +1,9 @@
+/**
+ example.js
+ */
+document.write("example.js 내부...");
+/*function printDate() {
+    document.write("example.js 내부...");
+    // document.getElementById("date").innerHTML = Date();
+}
+*/
